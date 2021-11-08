7790e490c71a616670f64089a9d4c5d54fadaa0f
Francisco <francisco.chagas@dcx.ufpb.br>
2021-11-08 10:27:33 -0300

    ID #23

--- quemsomos.php
+++ quemsomos.php
@@ -1,184 +1,125 @@
 <?php
+
 include("header.php");
 
 ?>
 
-
-<meta property="og:image" content="http://www.memoriajoaopessoa.com.br/img/facebook.png"/>
-<meta property="og:title" content="Quem Somos - Memória João Pessoa"/>
-<meta property="og:url" content="http://www.memoriajoaopessoa.com.br/formacao-e-evolucao.jsf"/>
-<meta property="og:site_name" content="Memória João Pessoa"/>
-<meta property="og:type" content="website"/>
 <title>Quem Somos - Memória João Pessoa</title>
 
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css" />
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+
 <style>
-	.titulo{
-		
-		color:white;
-
+	.hm-presentation {
+		text-align: center;
 	}
 
-</style>
-
-
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
-</script>
-
-
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.js"></script>
-<style>	
-	.round-border {
-		border: 1px solid #eee;
-		border: 1px solid rgba(0, 0, 0, 0.05);
-		-webkit-border-radius: 4px;
-		-moz-border-radius: 4px;
-		border-radius: 4px;
-		padding: 10px;
-		margin-bottom: 5px;
+	.hm-item {
+		margin: 40px 3%;
+		max-width: 167px !important;
+		width: 22%;
 	}
-	
-	#timeline
-	{
-		width: 100%;
-		background: #e61500;
-		height: 8px;
-		display: block;
-		margin: 50px 0;
-		-webkit-border-radius: 5px;
-		-moz-border-radius: 5px;
-		border-radius: 5px;
+
+	.hm-presentation img {
+		max-width: 100%;
 	}
-	.linha
-	{
-		list-style: none;
-		position: relative;
-		margin-top: -5px;
+
+	.hm-presentation img {
+		max-width: 100%;
 	}
-	.linha > li
-	{
-		width: 20px;
-		background: #e61500;
-		height: 20px;
-		-webkit-border-radius: 200px;
-		-moz-border-radius: 200px;
-		border-radius: 200px;
+
+	@media only screen and (min-width:1290px) and (max-width: 1420px) {
+		.caixaFoto {
+			margin-left: 3% !important;
+		}
+	}
+
+	@media only screen and (min-width:0px) and (max-width: 1140px) {
+		.caixaFoto {
+			width: 80px !important;
+			height: 80px !important;
+			margin-left: 10px !important;
+		}
+	}
+
+	@media only screen and (min-width:1141px) and (max-width: 1280px) {
+		.caixaFoto {
+			width: 100px !important;
+			height: 100px !important;
+			margin-left: 20px !important;
+		}
+	}
+
+	img.btnLogo {
+		margin-top: 10px;
 		float: left;
-		margin: -5px 7%;
-		cursor: pointer;
-		
-		-webkit-transition: all 0.5s ease;
-		-moz-transition: all 0.5s ease;
-		-o-transition: all 0.5s ease;
-		transition: all 0.5s ease;
-	}
-	
-	@media only screen and (min-width:686px) and (max-width: 1155px)
-	{
-		.linha > li
-		{
-			margin: -5px 6%;
-		}
-	}
-	
-	@media only screen and (min-width:1px) and (max-width: 686px)
-	{
-		.linha > li
-		{
-			margin: -5px 5%;
-		}
 	}
 
-	
-	.selected
-	{
-		border: 4px solid #e61500;
-		background-color: white !important;
+	.caixaFoto {
+		width: 120px;
+		height: 120px;
+		border-radius: 100px;
+		overflow: hidden;
+		float: left;
+		margin-left: 5%
 	}
-	
-	.ano {
-		position: absolute;
-		top: 25px;
-		margin-left: -10px;
-		font-family: 'Open Sans Condensed', sans-serif;
-		font-size: 22px;
+
+	.img-rounded {
+		max-width: 152px !important;
+		margin-left: -32px;
+
 	}
-	
-	p { text-align: justify;}
-	
+
+	.linha {
+		width: 100%;
+		overflow: hidden;	
+		margin-left: auto;
+    	margin-right: auto;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
 
 
 
 <div class="centerCT">
-	<h1>Quem somos</h1>
-
-    <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Foto</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Ano</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-          
-
-            require_once '../../../galaxy/c.php';
-  
-
-           $sql = "SELECT id,foto,nome,descricao FROM quemsomos";
-           
-        $q = $pdo->prepare($sql);
-        $q->execute(['%id']);
-         $q->execute(['%foto']);
-         $q->execute(['%nome']);
-         $q->execute(['%descricao']);
-        $q->setFetchMode(PDO::FETCH_ASSOC);
-
-
-               /* fetch values */
-               while ($r = $q->fetch()) {
-
-                   echo '
-                   <tr>
-      <td><img style = width:100px;height:150px src = "fotosquemsomos/'.$r["foto"].'" /></td>
-      <td>'.$r["nome"].'</td>
-      <td>'.$r["descricao"].'</td>
-    </tr>  
-
-                   ';
-               }
-
-
-
-
-
-
-           ?>
-    
-  </tbody>
-
-</table>
-
-			
-
-		</ul>
-		
-	</div>        	
-	
-	
-	<!-- End of Page -->
-	
+	<div  style="width: 100%;">
+		<div class="homeBlock">
+			<!-- O portal Memória João Pessoa --> 
+			<h1>Quem Somos</h1>
+			<p align="justify">Vigente desde 2006, o projeto de extensão “Memória João Pessoa” está vinculado ao Departamento de Arquitetura e Urbanismo da UFPB. Tem por objetivo contribuir para conscientização da sociedade quanto à conservação do patrimônio cultural da cidade de João Pessoa/PB utilizando, para tanto, a seguinte ferramenta de educação patrimonial: o website memoriajoaopessoa.com.br. Metodologicamente, o projeto segue linhas de atuação integradas e contínuas ao longo de toda vigência anual da ação, sendo fundamentais as linhas de produção de conteúdo e atualização do site. Enquanto a produção de conteúdo requer conhecimentos específicos dos discentes da graduação em Arquitetura e Urbanismo que integram a equipe, o trabalho de atualização do website depende de competências que são próprias dos discentes da informática. Desta forma, o projeto só alcança resultados quando ocorre a interdisciplinaridade entre essas duas áreas de conhecimento, uma vez que os conteúdos desenvolvidos para alimentar os diversos links do site podem, através da programação, chegar à sociedade através do website. </p>
+		</div>
+	</div>
 </div>
 
+<div class="centerCT">
+	<div  style="width: 100%;">
+		<div class="homeBlock">
+			<h1>Desenvolvedores</h1>
+		</div>
+	</div>
+</div>
+<div class="centerCT" >
 
+	<div class="linha">
+		<div class="caixaFoto post" data-ot="@franciscoo08"><a href="https://www.instagram.com/franciscoo08/"><img src="img/quemsomosnovo/1.png" /></a></div>
+		<div class="caixaFoto post" data-ot="@lucas.bk201"><a href="https://www.facebook.com/lucas.bk201"><img src="img/quemsomosnovo/2.1.png" /></a></div>
+		<div class="caixaFoto post" data-ot="@mariasaritaa"><a href="https://www.instagram.com/mariasaritaa/"><img src="img/quemsomosnovo/3.png" /></a></div>
+		<div class="caixaFoto post" data-ot="@marcyomacedo"><a href="https://www.instagram.com/marcyomacedo/"><img src="img/quemsomosnovo/4.png" /></a></div>
 
+	</div>
 
-
+</div>
 
 <?php
 include("footer.php");
-
 ?>
